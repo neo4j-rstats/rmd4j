afce6d77d7829d2e885af0c281d9163f117d2376
Colin <colin@thinkr.fr>
2018-03-01 18:12:33 +0100

    break in two funs

--- R/utils.R
+++ R/utils.R
@@ -2,15 +2,19 @@ clean_input <- function(vec){
   paste(gsub("\n", " ", vec), collapse = " ")
 }
 
-encypher_for_knitr <- function(options){
+encypher_options <- function(options){
   if (is.null(options$neo4j_passwd)){
-    out <- system( glue("echo '{options$code}' | {options$cypher_bin} -a {options$neo4j_adress} --format {options$neo4j_format}"),
-                   intern = TRUE)
+    out <- glue("echo '{options$code}' | {options$cypher_bin} -a {options$neo4j_adress} --format {options$neo4j_format}")
   } else {
-    out <- system( glue("echo '{options$code}' | {options$cypher_bin} -a {options$neo4j_adress} -u {options$neo4j_user} -p {options$neo4j_passwd} --format {options$neo4j_format}"),
-                   intern = TRUE)
+    out <- glue("echo '{options$code}' | {options$cypher_bin} -a {options$neo4j_adress} -u {options$neo4j_user} -p {options$neo4j_passwd} --format {options$neo4j_format}")
   }
   out
 }
+
+
+encypher_for_knitr <- function(options){
+  system( encypher_options(options), intern = TRUE)
+}
+
 
 
